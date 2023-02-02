8b84368b17c70ee4d4eb310cb01c4518b1ecbbff
Arsen <hoabs@mail.ru>
2023-02-02 19:55:25 +0300

    Added main function

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] ModifyStringArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] res = new string[count];
+    count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            res[count] = array[i];
+            count++;
+        }
+    }
+
+    return res;
+
+}
+
